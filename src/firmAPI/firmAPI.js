045820cbdeb98c373691dac7839e9e77c00f36b3
Trang Doan <doantranghp2000@gmail.com>
2022-10-01 16:16:29 -0300

    add reverse geocoding

--- src/firmAPI/firmAPI.js
+++ src/firmAPI/firmAPI.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch"
 
 async function getData() {
+  // Get the date of the day
   let date = new Date()
   const today = date.toISOString().split("T")[0]
   const url =
@@ -12,10 +13,47 @@ async function getData() {
     .then((res) => res.text())
     .then((text) => {
       today_data = text
-      console.log(typeof today_data)
     })
     .catch((err) => console.log(err))
-  console.log(typeof today_data)
+  // TODO: turn this to an array
+  return today_data
 }
 
-getData()
+async function checkIfInRosario(lat, lon) {
+  const url =
+    "https://nominatim.openstreetmap.org/reverse?format=json&lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&zoom=18&addressdetails=1"
+  let place_name
+  await fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.address)
+      const address = data.address
+      if (address.hasOwnProperty("state")) {
+        if (address.state == "Santa Fe") {
+          if (address.hasOwnProperty("city")) {
+            if (address.city == "Rosario") {
+              place_name = data.display_name
+            }
+          }
+        }
+      }
+    })
+
+  if (place_name) {
+    return [true, place_name]
+  } else {
+    return [false, "the place is not in Rosario"]
+  }
+}
+
+async function createDatasetToday() {
+  const today_data = getData()
+}
+const check = await checkIfInRosario("-32.991423", "-60.712546")
+console.log(check)
+
+// today_data = getData()
