00f13ab37fd7d4e66ab54e53d74e792d419a7154
lMartin3 <me@martinl.dev>
2022-10-02 00:20:03 -0300

    feat: basic menu functionality

--- src/whatsapp/whatsapp_bot.js
+++ src/whatsapp/whatsapp_bot.js
@@ -1,6 +1,14 @@
 import {create, Client, QRFormat, QRQuality } from "@open-wa/wa-automate";
+import loc from "./localization/es_AR.json" assert { type: 'json' }; // this assertion is required
+const ST_MAIN_MENU = "main";
+const STATUS_B = "";
 
 let client;
+
+/*
+    Warning: Potential memory leak; should clean every now and then or restart the bot instance.
+*/
+var chatStatus = {}
 
 
 function setup() {
@@ -10,6 +18,10 @@ function setup() {
         authTimeout: 0,
         cacheEnabled: false,
         cachedPatch: true,
+        /*
+            These flags help maintain stability,
+            and also prevent reconnection issues.
+        */
         chromiumArgs: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
@@ -41,6 +53,48 @@ async function start(whatsappClient) {
         console.log(`Chat ${i}: ${chats[i].id}`)
     }
     await client.sendText("120363044984461166@g.us", "hello!");
+
+    client.onMessage(message=>{
+        console.log(message.body);
+        console.log(chatStatus[message.chatId] ?? "none");
+        if(!chatStatus[message.chatId]) {
+            chatStatus[message.chatId] = ST_MAIN_MENU;
+            client.sendText(message.chatId, loc.welcomeScreen);
+            client.sendText(message.chatId, loc.initialMenuChoices);
+        } else {
+            switch(chatStatus[message.chatId]) {
+                case ST_MAIN_MENU:
+                    var opt = parseInt(message.body);
+                    var simpleMessages = ["","", loc.getInformation, loc.getInvolved, loc.donate, loc.submitSympthoms]
+                    switch(opt) {
+                        case 1:
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                            client.sendText(message.chatId, simpleMessages[opt]);
+                            break;
+                        case 6:
+                            //TODO verify if the user is actually signed up
+                            if(2+2==4) {
+                                client.sendText(message.chatId, loc.successfullyUnsubscribed);
+                            } else {
+                                client.sendText(message.chatId, loc.notSubscribed);
+                            }
+                            break;
+                        default:
+                            client.sendText(message.chatId, loc.invalidChoice);
+                            client.sendText(message.chatId, loc.initialMenuChoices);
+                            break;
+                    }
+                    break;
+            }
+        }
+    });
+
+
+    
 }
 
 
