d0d0cbf290e8d2b2b32eab2bd4e388af122a39bf
Trang Doan <doantranghp2000@gmail.com>
2022-10-02 13:44:33 -0300

    add squares to define wetland regions

--- src/firmAPI/firmAPI.js
+++ src/firmAPI/firmAPI.js
@@ -29,8 +29,8 @@ async function getData() {
 function checkIfInRosario(lat, lon, regions) {
   // check if the data point is within the wetland region or not
   regions.forEach((region) => {
-    const xRange = (region.topLeft[1], region.botRight[1])
-    const yRange = (region.topLeft[0], region.botRight[0])
+    const xRange = (region[1][1], region[0][1])
+    const yRange = (region[1][0], region[0][0])
     if (yRange[0] <= lat <= yRange[1] && xRange[0] <= lon <= xRange[1]) {
       return true
     }
@@ -40,8 +40,27 @@ function checkIfInRosario(lat, lon, regions) {
 
 async function createDatasetToday() {
   // Filter out only Rosario wetland data from all Argentine fire data
-  const regions = [{ topLeft: (-1, -2), botRight: (-3, 4) }]
+  const regions = [
+    [
+      [-33.39087482955327, -58.50674748160459],
+      [-34.387524974821325, -59.123293544400354],
+    ],
+    [
+      [-33.19934046720891, -59.13810756104891],
+      [-33.83221610724139, -60.171178725618375],
+    ],
+    [
+      [-32.612518268000905, -60.2006744136563],
+      [-33.0733681732123, -60.64234700361604],
+    ],
+    [
+      [-31.886847558311302, -60.51245376437194],
+      [-32.69620288634272, -60.75438881783346],
+    ],
+  ]
   const today_data = await getData()
+  console.log("today data is ")
+  console.log(today_data)
   let dataset = []
   today_data.forEach((ele) => {
     const lat = ele[1]
@@ -57,4 +76,5 @@ async function createDatasetToday() {
 
 // getData()
 
-// createDatasetToday()
+const today_dataset = await createDatasetToday()
+console.log(today_dataset)
